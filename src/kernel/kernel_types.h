1f42a5485d4aaba4ef661145c668540bdce7ec77
Alex <boberfly@gmail.com>
2021-07-09 15:14:16 -0700

    Fixed missing RTC_NAMESPACE

--- src/kernel/kernel_types.h
+++ src/kernel/kernel_types.h
@@ -1477,7 +1477,7 @@ typedef struct KernelBVH {
   OptixTraversableHandle scene;
 #else
 #  ifdef __EMBREE__
-  RTCScene scene;
+  RTC_NAMESPACE::RTCScene scene;
 #    ifndef __KERNEL_64_BIT__
   int pad2;
 #    endif
