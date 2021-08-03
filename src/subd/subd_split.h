e83eba899be15c6d61ca737169aa236d0514cac9
Piotr Barejko <piotrbarejko@protonmail.com>
2021-08-03 10:21:19 -0700

    SubdParams const correctness

--- src/subd/subd_split.h
+++ src/subd/subd_split.h
@@ -37,7 +37,7 @@ class Mesh;
 class Patch;
 
 class DiagSplit {
-  SubdParams params;
+  const SubdParams params;
 
   vector<Subpatch> subpatches;
   /* deque is used so that element pointers remain vaild when size is changed. */
